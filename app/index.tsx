104dede1716b83174002f9fae08e65024149c851
ekduszla <dayeonkim0228@gmail.com>
2025-08-14 10:30:07 +0900

    기본 컴포넌트, 스타일 컴포넌트 사용방법

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,9 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+// View : 전체 감싸는 컨테이너
+// Text : 텍스트 표시 컴포넌트 
+// - text 작성시 text 감싸는 Text 컴포넌트 사용
+// style : 스타일 적용 props
 
 export default function Index() {
   return (
@@ -9,7 +14,28 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello world.</Text>
+      <Text style = {styles.mainText}>Hello world.</Text>
+      <Text style = {{fontSize: 20,
+      fontWeight:"bold",
+      color: "pink",
+      borderWidth : 1,
+      borderColor : "green",
+      padding : 10,
+      margin: 10,
+      borderRadius: 10,}}>Hello world.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    borderWidth : 1,
+    borderColor : "blue",
+    padding : 10,
+    margin: 10,
+    borderRadius: 10,
+
+  },
+});
